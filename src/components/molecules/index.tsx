1fb4b151858d32b9b7ce85c3cbf550ca7958d0d6
FAJRIKUN-MAKALALAG <S22210271@student.unklab.ac.id>
2025-11-19 01:00:56 +0800

    initial commit

--- src/components/molecules/index.tsx
+++
@@ -1,6 +0,0 @@
-import Header from './Header';
-import TextInput from './TextInput';
-import InputGemini from './InputGemini';
-import HeaderChat from './HeaderChat';
-
-export {Header, TextInput, InputGemini, HeaderChat};
